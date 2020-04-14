bee41c599bb79a7f951482344cb370d4bdebb1ee
Rutger Vos <rutgeraldo@gmail.com>
2020-04-14 11:45:44 +0200

    fixed namespace

--- lib/Bio/Phylo/CIPRES.pm
+++ lib/Bio/Phylo/CIPRES.pm
@@ -1,11 +1,11 @@
-package Bio::CIPRES;
+package Bio::Phylo::CIPRES;
 use strict;
 use warnings;
 our $VERSION="0.0.1";
 
 =head1 NAME
 
-Bio::CIPRES - Client for the CIPRES analysis portal
+Bio::Phylo::CIPRES - Client for the CIPRES analysis portal
 
 =cut
 
